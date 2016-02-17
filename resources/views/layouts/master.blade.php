f3ad33b9b5c434c3d5e0b410e78e0d7f732b9c58
Diego Lorenzo <diego@diegolorenzo.com>
2016-02-17 10:50:07 -0500

    Adding Forge pattern library and updating some views.

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -9,21 +9,31 @@
         <title>DoSomething Competitions</title>
 
         <link rel="icon" type="image/ico" href="/favicon.ico?v1">
-        <link rel="stylesheet" href="">
+
+        <script src="{{ asset('/assets/vendor/forge/modernizr.js') }}"></script>
+
+        <link rel="stylesheet" href="{{ asset('assets/vendor/forge/forge.css') }}">
+        <link rel="stylesheet" href="{{ asset('assets/vendor/modal/modal.css') }}">
     </head>
 
     <body>
-        <div id="page" class="page">
+        <div class="chrome">
+            <div class="wrapper">
 
-            <div class="container">
+                <div class="container">
 
-                @yield('main_content')
+                    @yield('main_content')
+
+                </div>
+
+                {{-- @include('layouts.navigation') // Commenting out for now. --}}
 
             </div>
-
-            @include('layouts.navigation')
         </div>
 
+        <script src="{{ asset('/assets/vendor/forge/forge.js') }}"></script>
+        <script src="{{ asset('/assets/vendor/modal/modal.js') }}"></script>
     </body>
 
 </html>
+
