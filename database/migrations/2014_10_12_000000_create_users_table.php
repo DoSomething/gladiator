885a15a48e9879cefa9d0aa4736c6c5a297c1e59
Diego Lorenzo <diego@diegolorenzo.com>
2016-02-18 13:20:05 -0500

    Update date to datetime and make role nullable.

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,7 +14,7 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->string('id')->primary();
-            $table->string('role');
+            $table->string('role')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
