11ff664fd70624f650dcde32bdd9f2ce4c94729a
Shae Smith <ssmith@dosomething.org>
2016-02-24 11:31:08 -0500

    fix the homepage

--- resources/views/errors/status.blade.php
+++ resources/views/errors/status.blade.php
@@ -2,7 +2,7 @@
     <div>{{ Session::get('status') }}</div>
 @endif
 
-@if($errors->any())
+@if(isset($errors) && $errors->any())
     <div class="alert alert-danger">
         @foreach($errors->all() as $error)
             <p>{{ $error }}</p>
