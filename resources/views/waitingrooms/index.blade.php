2b876359489e2fa64bf2ff092aa9c7b77146c911
Shae Smith <ssmith@dosomething.org>
2016-02-29 11:44:13 -0500

    responsive styles for tables

--- resources/views/waitingrooms/index.blade.php
+++ resources/views/waitingrooms/index.blade.php
@@ -17,29 +17,30 @@
     <div class="container">
         <div class="wrapper">
             <div class="container__block">
-
-                <table id="user-table" class="table">
-                  <thead>
-                    <tr class="row table-header">
-                      <th class="table-cell">ID</th>
-                      <th class="table-cell">Campaign</th>
-                      <th class="table-cell">Campaign Run</th>
-                      <th class="table-cell">Start Date</th>
-                      <th class="table-cell">End Date</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    @foreach($rooms as $room)
-                      <tr class="table-row">
-                        <td class="table-cell"><a href="{{ route('waitingrooms.show', $room->id) }}">{{ $room->id }}</a></td>
-                        <td class="table-cell">{{ $room->campaign_id }}</td>
-                        <td class="table-cell">{{ $room->campaign_run_id }}</td>
-                        <td class="table-cell">{{ date('F d, Y', strtotime($room->signup_start_date)) }}</td>
-                        <td class="table-cell">{{ date('F d, Y', strtotime($room->signup_end_date)) }}</td>
-                      </tr>
-                    @endforeach
-                  </tbody>
-                </table>
+                <div class="table-responsive">
+                    <table class="table">
+                      <thead>
+                        <tr class="table-header">
+                          <th class="table-cell">ID</th>
+                          <th class="table-cell">Campaign</th>
+                          <th class="table-cell">Campaign Run</th>
+                          <th class="table-cell">Start Date</th>
+                          <th class="table-cell">End Date</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        @foreach($rooms as $room)
+                          <tr class="table-row">
+                            <td class="table-cell"><a href="{{ route('waitingrooms.show', $room->id) }}">{{ $room->id }}</a></td>
+                            <td class="table-cell">{{ $room->campaign_id }}</td>
+                            <td class="table-cell">{{ $room->campaign_run_id }}</td>
+                            <td class="table-cell">{{ date('F d, Y', strtotime($room->signup_start_date)) }}</td>
+                            <td class="table-cell">{{ date('F d, Y', strtotime($room->signup_end_date)) }}</td>
+                          </tr>
+                        @endforeach
+                      </tbody>
+                    </table>
+                </div>
             </div>
         </div>
     </div>
