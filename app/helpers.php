21dc2059ae3e34db9cc3327d840c539dc177d655
Diego Lorenzo <diego@diegolorenzo.com>
2016-03-01 11:27:19 -0500

    Removing wrapping function_exists since we want our funcs to be used.

--- app/helpers.php
+++ app/helpers.php
@@ -3,72 +3,64 @@
 use Gladiator\Models\User;
 use Gladiator\Services\Northstar\Exceptions\NorthstarUserNotFoundException;
 
-if (! function_exists('matchEmailDomain')) {
-    /**
-     * Match supplied email against a specific domain or default domain.
-     *
-     * @param  string  $email
-     * @param  string  $domain
-     * @return bool
-     */
-    function matchEmailDomain($email, $domain = 'dosomething.org')
-    {
-        $pieces = explode('@', $email);
+/**
+ * Match supplied email against a specific domain or default domain.
+ *
+ * @param  string  $email
+ * @param  string  $domain
+ * @return bool
+ */
+function matchEmailDomain($email, $domain = 'dosomething.org')
+{
+    $pieces = explode('@', $email);
 
-        if (is_array($pieces) && count($pieces) > 1) {
-            return $pieces[1] === $domain ? true : false;
-        }
-
-        return false;
+    if (is_array($pieces) && count($pieces) > 1) {
+        return $pieces[1] === $domain ? true : false;
     }
+
+    return false;
 }
 
-if (! function_exists('findUserAccountInSystem')) {
-    /**
-     * @param  string  $id
-     * @param  string  $email
-     * @return \Gladiator\Models\User|string
-     * @throws \Gladiator\Services\Northstar\Exceptions\NorthstarUserNotFoundException
-     */
-    function findUserAccountInSystem($id, $type = 'email')
-    {
-        $northstarUser = findNorthstarAccount($id, $type);
+/**
+ * @param  string  $id
+ * @param  string  $email
+ * @return \Gladiator\Models\User|string
+ * @throws \Gladiator\Services\Northstar\Exceptions\NorthstarUserNotFoundException
+ */
+function findUserAccountInSystem($id, $type = 'email')
+{
+    $northstarUser = findNorthstarAccount($id, $type);
 
-        if (! $northstarUser) {
-            throw new NorthstarUserNotFoundException;
-        }
-
-        $gladiatorUser = findGladiatorAccount($northstarUser->id);
-
-        if (! $gladiatorUser) {
-            return $northstarUser->id;
-        }
-
-        return $gladiatorUser;
+    if (! $northstarUser) {
+        throw new NorthstarUserNotFoundException;
     }
+
+    $gladiatorUser = findGladiatorAccount($northstarUser->id);
+
+    if (! $gladiatorUser) {
+        return $northstarUser->id;
+    }
+
+    return $gladiatorUser;
 }
 
-if (! function_exists('findGladiatorAccount')) {
-    /**
-     * @param  string  $id
-     * @return \Gladiator\Models\User|null
-     */
-    function findGladiatorAccount($id)
-    {
-        return User::find($id);
-    }
+/**
+ * @param  string  $id
+ * @return \Gladiator\Models\User|null
+ */
+function findGladiatorAccount($id)
+{
+    return User::find($id);
 }
 
-if (! function_exists('findNorthstarAccount')) {
-    /**
-     * @param  string  $id
-     * @param  string  $type
-     * @return object|null
-     */
-    function findNorthstarAccount($id, $type = 'email')
-    {
-        $northstar = app('northstar');
+/**
+ * @param  string  $id
+ * @param  string  $type
+ * @return object|null
+ */
+function findNorthstarAccount($id, $type = 'email')
+{
+    $northstar = app('northstar');
 
-        return $northstar->getUser($id, $type);
-    }
+    return $northstar->getUser($id, $type);
 }
