f267ad0c16a61c6f37f036cf62c72b2602552795
deadlybutter <thedeadlybutter@gmail.com>
2016-03-03 11:22:17 -0500

    Adds waiting room api function

---
+++ app/Http/Controllers/Api/WaitingRoomsController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Gladiator\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use Gladiator\Http\Controllers\Controller;
+use Gladiator\Models\WaitingRoom;
+
+class WaitingRoomsController extends Controller
+{
+
+    /**
+     * Get a waiting room for the specified campaign ID
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function getWaitingRoom(Request $request)
+    {
+        $this->validate($request, [
+            'campaign_id' => 'required|integer'
+        ]);
+
+        $campaign_id = $request->input('campaign_id');
+        $waiting_room = WaitingRoom::find($campaign_id);
+
+        return response()->json($waiting_room, 201);
+    }
+}
