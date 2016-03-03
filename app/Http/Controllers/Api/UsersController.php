f9dc578cead0168d7a1b20956f24866d17aa4216
Diego Lorenzo <diego@diegolorenzo.com>
2016-03-03 10:38:19 -0500

    Adding Repositories and Api Controllers.

---
+++ app/Http/Controllers/Api/UsersController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Gladiator\Http\Controllers\Api;
+
+use Gladiator\Repositories\UserRepository;
+use Gladiator\Http\Controllers\Controller;
+
+class UsersController extends Controller
+{
+
+    // @TODO: set up a UserRepository to handle a lot of the business logic
+    // that would be shared between the Web UsersController and Api UsersController
+
+
+    public function store()
+    {
+        // do stuff.
+    }
+}
