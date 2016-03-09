0c45f86f6f76d14925057ad160cd61057b51bd8b
deadlybutter <thedeadlybutter@gmail.com>
2016-03-09 14:57:12 -0500

    Switches to old man, I mean old() way of doing forms

--- resources/views/contests/partials/_form_contest.blade.php
+++ resources/views/contests/partials/_form_contest.blade.php
@@ -2,17 +2,17 @@
 
 <div class="form-item -padded">
     {!! Form::label('campaign_id', 'Campaign ID:', ['class' => 'field-label']) !!}
-    {!! Form::text('campaign_id', (isset($contest->campaign_id)) ? $contest->campaign_id : NULL, ['class' => 'text-field']) !!}
+    {!! Form::text('campaign_id', old('campaign_id'), ['class' => 'text-field']) !!}
 </div>
 
 <div class="form-item -padded">
     {!! Form::label('campaign_run_id', 'Run ID:', ['class' => 'field-label']) !!}
-    {!! Form::text('campaign_run_id', (isset($contest->campaign_run_id)) ? $contest->campaign_run_id : NULL, ['class' => 'text-field']) !!}
+    {!! Form::text('campaign_run_id', old('campaign_run_id'), ['class' => 'text-field']) !!}
 </div>
 
 <div class="form-item -padded">
     {!! Form::label('duration', 'Duration (in days):', ['class' => 'field-label']) !!}
-    {!! Form::number('duration', (isset($contest->duration)) ? $contest->duration : NULL, ['class' => 'text-field']) !!}
+    {!! Form::number('duration', old('duration'), ['class' => 'text-field']) !!}
 </div>
 
 <div class="form-item -padded">
