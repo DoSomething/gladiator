4c0f362e617caa5b5f4a24e39d6bda4501969c68
deadlybutter <thedeadlybutter@gmail.com>
2016-03-11 14:18:47 -0500

    Fixes transform to be better looking

--- app/Http/Transformers/ContestTransformer.php
+++ app/Http/Transformers/ContestTransformer.php
@@ -17,11 +17,15 @@ class ContestTransformer extends TransformerAbstract
     {
         return [
             'id' => $contest->id,
-            'campaign_id' => $contest->campaign_id,
-            'campaign_run_id' => $contest->campaign_run_id,
-            'signups_open' => $contest->waitingRoom->isOpen(),
-            'signup_start' => $contest->waitingRoom->signup_start_date->toIso8601String(),
-            'signup_end' => $contest->waitingRoom->signup_end_date->toIso8601String(),
+            'campaign' => [
+                'campaign_id' => $contest->campaign_id,
+                'campaign_run_id' => $contest->campaign_run_id,
+            ],
+            'waiting_room' => [
+                'signups_open' => $contest->waitingRoom->isOpen(),
+                'signup_start' => $contest->waitingRoom->signup_start_date->toIso8601String(),
+                'signup_end' => $contest->waitingRoom->signup_end_date->toIso8601String(),
+            ]
         ];
     }
 }
