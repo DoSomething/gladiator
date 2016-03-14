3e70ae7978e2fb8b93ea431594526f31adc1c79d
deadlybutter <thedeadlybutter@gmail.com>
2016-03-14 14:10:02 -0400

    Adds update logic

--- app/Http/Controllers/ContestsController.php
+++ app/Http/Controllers/ContestsController.php
@@ -76,7 +76,10 @@ class ContestsController extends Controller
      */
     public function update(ContestRequest $request, Contest $contest)
     {
-        $contest->fill($request->all())->save();
+        $dateParams = ['signup_start_date', 'signup_end_date'];
+
+        $contest->fill($request->except($dateParams))->save();
+        $contest->WaitingRoom->fill($request->only($dateParams))->save();
 
         return redirect()->back()->with('status', 'Contest has been updated!');
     }
