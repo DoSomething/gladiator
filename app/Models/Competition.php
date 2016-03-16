d1391f382192ba0df830f561aa49bfeee7ca2721
deadlybutter <thedeadlybutter@gmail.com>
2016-03-16 15:42:30 -0400

    Cleans up the model :broom:

--- app/Models/Competition.php
+++ app/Models/Competition.php
@@ -27,25 +27,4 @@ class Competition extends Model
     {
         return $this->belongsToMany(User::class);
     }
-
-    /**
-     * Gets a CSV export of all the users in this competition.
-     *
-     * @param \Gladiator\Repositories\UserRepositoryContract $repository
-     * @return \League\Csv\ $csv
-     */
-    public function getCSVExport($repository)
-    {
-        $data = [];
-        $users = $this->users;
-
-        // TODO: Competition/Reportback activity
-        array_push($data, ['northstar_id', 'first_name', 'last_name', 'email', 'cell']);
-        foreach ($users as $user) {
-            $userInfo = $repository->find($user->id);
-            array_push($data, [$user->id, $userInfo->first_name, $userInfo->last_name, $userInfo->email, $userInfo->mobile]);
-        }
-
-        return buildCSV($data);
-    }
 }
--- app/Models/WaitingRoom.php
+++ app/Models/WaitingRoom.php
@@ -29,26 +29,6 @@ class WaitingRoom extends Model
         return $this->belongsToMany(User::class);
     }
 
-    /**
-     * Gets a CSV export of all the users in this waiting room.
-     *
-     * @param \Gladiator\Repositories\UserRepositoryContract $repository
-     * @return \League\Csv\ $csv
-     */
-    public function getCSVExport($repository)
-    {
-        $data = [];
-        $users = $this->users;
-
-        array_push($data, ['northstar_id', 'first_name', 'last_name', 'email', 'cell']);
-        foreach ($users as $user) {
-            $userInfo = $repository->find($user->id);
-            array_push($data, [$user->id, $userInfo->first_name, $userInfo->last_name, $userInfo->email, $userInfo->mobile]);
-        }
-
-        return buildCSV($data);
-    }
-
     /*
      * Determines if the waiting room is open for signupsOpen
      *
