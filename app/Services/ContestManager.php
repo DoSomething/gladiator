5723f2df73809fb09bd23750dcfb76bbfd975c03
deadlybutter <thedeadlybutter@gmail.com>
2016-03-16 15:48:10 -0400

    Adds doc block

--- app/Services/ContestManager.php
+++ app/Services/ContestManager.php
@@ -23,13 +23,20 @@ class ContestManager
         $this->repository = $repository;
     }
 
-    public function exportCSV($model, $includeReportbackData = false)
+    /**
+     * Build CSV data for a given WaitingRoom or Competition
+     *
+     * @param WaitingRoom|Competition $model
+     * @param bool $reportbacks Should this csv include reportback data?
+     * @return \League\Csv $csv
+     */
+    public function exportCSV($model, $reportbacks = false)
     {
         $data = [];
         $users = $model->users;
 
         $headers = ['northstar_id', 'first_name', 'last_name', 'email', 'cell'];
-        if ($includeReportbackData) {
+        if ($reportbacks) {
             //TODO
             array_push($headers, 'some reportback headers go here');
         }
@@ -39,7 +46,7 @@ class ContestManager
         foreach ($users as $user) {
             $userInfo = $this->repository->find($user->id);
             $details = [$user->id, $userInfo->first_name, $userInfo->last_name, $userInfo->email, $userInfo->mobile];
-            if ($includeReportbackData) {
+            if ($reportbacks) {
                 //TODO
                 array_push($headers, 'some reportback details go here');
             }
