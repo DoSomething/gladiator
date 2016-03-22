b4d06ad82594e7eee1d2cff76ca605316b13da65
Shae Smith <ssmith@dosomething.org>
2016-03-22 14:45:36 -0400

    Remove duration column from contest table

---
+++ database/migrations/2016_03_22_175301_drop_contest_duration_column.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class DropContestDurationColumn extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('contests', function ($table) {
+            $table->dropColumn('duration');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('contests', function (Blueprint $table) {
+            $table->integer('duration')->comment = 'The duration of a competition within this contest in units of days';
+        });
+    }
+}
