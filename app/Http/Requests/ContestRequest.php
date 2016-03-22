d09928943fdb4207f48f2a49f6b07bc742639d58
Shae Smith <ssmith@dosomething.org>
2016-03-22 16:00:54 -0400

    Update contest model, views, and requests to not reference the duration field anymore

--- app/Http/Requests/ContestRequest.php
+++ app/Http/Requests/ContestRequest.php
@@ -24,7 +24,6 @@ class ContestRequest extends Request
         return [
             'campaign_id' => 'required|numeric',
             'campaign_run_id' => 'required|numeric',
-            'duration' => 'required|numeric',
             'signup_start_date' => 'required|date',
             'signup_end_date' => 'required|date',
         ];
