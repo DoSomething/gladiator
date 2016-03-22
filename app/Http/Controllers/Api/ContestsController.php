3b8acce15ae95526cb0afbedc24f81be1d9a999f
Diego Lorenzo <diego@diegolorenzo.com>
2016-03-22 14:10:29 -0400

    Updating Contest controller and API docs.

--- app/Http/Controllers/Api/ContestsController.php
+++ app/Http/Controllers/Api/ContestsController.php
@@ -22,16 +22,28 @@ class ContestsController extends ApiController
     public function __construct()
     {
         $this->transformer = new ContestTransformer;
+
+        $this->middleware('auth.api');
     }
 
     /**
      * Get a collection of contests
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
      */
     public function index(Request $request)
     {
-        $campaign_run_nid = $request->query('run_nid');
-        if (isset($campaign_run_nid)) {
-            $contest = Contest::with('waitingRoom')->where('campaign_run_id', $campaign_run_nid)->firstOrFail();
+        $campaignRunId = $request->query('campaign_run_id');
+
+        // @TODO: below is temporary fix until Phoenix GET request updates run_nid param to run_id.
+        // We want to aim to not have any proprietary Drupal id names :P
+        if (!$campaignRunId) {
+            $campaignRunId = $request->query('run_nid');
+        }
+
+        if (isset($campaignRunId)) {
+            $contest = Contest::with('waitingRoom')->where('campaign_run_id', $campaignRunId)->firstOrFail();
 
             return $this->item($contest);
         }
