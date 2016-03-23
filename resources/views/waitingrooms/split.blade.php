383cc43d6d4bf70d28d1b78c20a304c54e2ee048
Shae Smith <ssmith@dosomething.org>
2016-03-23 12:47:29 -0400

    put proposed competition splits into a table

--- resources/views/waitingrooms/split.blade.php
+++ resources/views/waitingrooms/split.blade.php
@@ -16,9 +16,28 @@
                     <input type="submit" class="button" value="Split" />
                 </form>
             </div>
+
+            <div class="container__block">
+                <h1>Proposed Competitions</h1>
+                <table class="table">
+                    <thead>
+                        <tr class="table__header">
+                            <th class="table__cell">Competition</th>
+                            <th class="table__cell">Total Users</th>
+                            <th class="table__cell">Set End Date</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                       @foreach($split as $key => $competition)
+                            <tr class="table__row">
+                                <td class="table__cell">{{ $key + 1 }}</td>
+                                <td class="table__cell">{{ $competition->count() }}</td>
+                                <td class="table__cell">something</td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
         </div>
     </div>
-    @foreach($split as $key => $competition)
-        @include('users.partials._table_users', ['users' => $competition, 'role' => 'Competition ' . ($key + 1) . ' : ' . $competition->count() . ' users'])
-    @endforeach
 @stop
