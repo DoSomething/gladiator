ef299a2de8389dd8f227b0eae0a3b4cee860af7b
Shae Smith <ssmith@dosomething.org>
2016-03-23 14:25:21 -0400

    Split form view updates

--- resources/views/waitingrooms/split.blade.php
+++ resources/views/waitingrooms/split.blade.php
@@ -6,40 +6,44 @@
         'title' => 'Waiting Room Banana Split'
     ])
 
-    <form method="POST" action="{{ route('split', $room->id) }}">
-        {{ method_field('POST') }}
-        {{ csrf_field() }}
-        <div class="container">
-            <div class="wrapper">
-                <div class="container__block -narrow">
-                    <div class="form-item -padded">
-                        <label class="field-label" for="competition_end_date">End date for these competitions:</label>
-                        <input type="date" name="competition_end_date" id="competition_end_date" value='MM/DD/YYYY' class="text-field"></input>
+    <div class="container">
+        <div class="wrapper">
+            <div class="container__block">
+                <h1>{{ count($split) . ' Proposed Competition(s)' }}</h1>
+            </div>
+            @if (count($split))
+                <form method="POST" action="{{ route('split', $room->id) }}">
+                    {{ method_field('POST') }}
+                    {{ csrf_field() }}
+                    <div class="container__block">
+                        <div class="form-item -padded">
+                            <label class="field-label" for="competition_end_date">End date for these competitions:</label>
+                            <input type="date" name="competition_end_date" id="competition_end_date" value='MM/DD/YYYY' class="text-field"></input>
+                        </div>
+
+                        <input type="submit" class="button" value="Split" />
                     </div>
 
-                    <input type="submit" class="button" value="Split" />
-                </div>
-
-                <div class="container__block">
-                    <h1>{{ count($split) . ' Proposed Competition(s)' }}</h1>
-                    <table class="table">
-                        <thead>
-                            <tr class="table__header">
-                                <th class="table__cell">Competition</th>
-                                <th class="table__cell">Total Users</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                           @foreach($split as $key => $competition)
-                                <tr class="table__row">
-                                    <td class="table__cell">{{ $key + 1 }}</td>
-                                    <td class="table__cell">{{ count($competition) }}</td>
+                    <div class="container__block">
+                        <table class="table">
+                            <thead>
+                                <tr class="table__header">
+                                    <th class="table__cell">Competition</th>
+                                    <th class="table__cell"># of Contestants</th>
                                 </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
-                </div>
-            </div>
+                            </thead>
+                            <tbody>
+                               @foreach($split as $key => $competition)
+                                    <tr class="table__row">
+                                        <td class="table__cell">{{ $key }}</td>
+                                        <td class="table__cell">{{ count($competition) }}</td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </form>
+            @endif
         </div>
-    </form>
+    </div>
 @stop
