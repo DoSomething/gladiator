1a9450ea00439df6a8f246460f0486d12e280f00
Diego Lorenzo <diego@diegolorenzo.com>
2016-03-28 15:28:28 -0400

    Adding migration to update contest and messages tables.

---
+++ database/migrations/2016_03_28_192628_add_sender_column_to_contests_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddSenderColumnToContestsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('contests', function ($table) {
+            $table->string('sender')->nullable()->after('campaign_run_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('contests', function ($table) {
+            $table->dropColumn('sender');
+        });
+    }
+}
