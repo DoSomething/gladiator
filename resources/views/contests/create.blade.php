abf9e4e227360678535a745ac78092a3c66049cf
Diego Lorenzo <diego@diegolorenzo.com>
2016-03-29 10:44:02 -0400

    Update to pass default messages to message creation fields.

--- resources/views/contests/create.blade.php
+++ resources/views/contests/create.blade.php
@@ -1,3 +1,5 @@
+@inject('defaultMessage', 'Gladiator\Http\Utilities\DefaultMessage')
+
 @extends('layouts.master')
 
 @section('main_content')
@@ -20,23 +22,23 @@
 
                     <h2 class="heading -alpha">Messaging Settings</h2>
 
-                    @include('contests.partials._form_contest_messaging', ['type' => 'welcome', 'key' => '1', 'label' => 'Welcome'])
+                    @include('contests.partials._form_contest_messaging', ['default' => $defaultMessage->get('welcome', '1'), 'type' => 'welcome', 'key' => '1'])
 
-                    @include('contests.partials._form_contest_messaging', ['type' => 'checkin', 'key' => '1', 'label' => 'Where are you? Check status'])
+                    @include('contests.partials._form_contest_messaging', ['default' => $defaultMessage->get('checkin', '1'), 'type' => 'checkin', 'key' => '1'])
 
-                    @include('contests.partials._form_contest_messaging', ['type' => 'reminder', 'key' => '1', 'label' => 'Reminder for first submission due'])
+                    @include('contests.partials._form_contest_messaging', ['default' => $defaultMessage->get('reminder', '1'), 'type' => 'reminder', 'key' => '1'])
 
-                    @include('contests.partials._form_contest_messaging', ['type' => 'reminder', 'key' => '2', 'label' => 'Reminder to submit v1'])
+                    @include('contests.partials._form_contest_messaging', ['default' => $defaultMessage->get('reminder', '2'), 'type' => 'reminder', 'key' => '2'])
 
-                    @include('contests.partials._form_contest_messaging', ['type' => 'reminder', 'key' => '3', 'label' => 'Reminder to submit v2'])
+                    @include('contests.partials._form_contest_messaging', ['default' => $defaultMessage->get('reminder', '3'), 'type' => 'reminder', 'key' => '3'])
 
-                    @include('contests.partials._form_contest_messaging', ['type' => 'reminder', 'key' => '4', 'label' => 'Last minute reminder v1'])
+                    @include('contests.partials._form_contest_messaging', ['default' => $defaultMessage->get('reminder', '4'), 'type' => 'reminder', 'key' => '4'])
 
-                    @include('contests.partials._form_contest_messaging', ['type' => 'reminder', 'key' => '5', 'label' => 'Last minute reminder v2'])
+                    @include('contests.partials._form_contest_messaging', ['default' => $defaultMessage->get('reminder', '5'), 'type' => 'reminder', 'key' => '5'])
 
-                    @include('contests.partials._form_contest_messaging', ['type' => 'leaderboard', 'key' => '1', 'label' => 'Leaderboard update'])
+                    @include('contests.partials._form_contest_messaging', ['default' => $defaultMessage->get('leaderboard', '1'), 'type' => 'leaderboard', 'key' => '1'])
 
-                    @include('contests.partials._form_contest_messaging', ['type' => 'leaderboard', 'key' => '2', 'label' => 'Final leaderboard update'])
+                    @include('contests.partials._form_contest_messaging', ['default' => $defaultMessage->get('leaderboard', '2'), 'type' => 'leaderboard', 'key' => '2'])
 
                     <input type="submit" class="button" value="Submit" />
                 </form>
