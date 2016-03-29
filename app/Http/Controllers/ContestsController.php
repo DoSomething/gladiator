32b1cf6dd4772b7cb2651918f6ef87e937475e3a
Diego Lorenzo <diego@diegolorenzo.com>
2016-03-29 15:04:44 -0400

    StyleCI fixes.

--- app/Http/Controllers/ContestsController.php
+++ app/Http/Controllers/ContestsController.php
@@ -50,7 +50,6 @@ class ContestsController extends Controller
      */
     public function store(ContestRequest $request)
     {
-
         $contest = Contest::create([
             'campaign_id' => $request->input('campaign_id'),
             'campaign_run_id' => $request->input('campaign_run_id'),
