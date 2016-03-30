c99259dc371a118c3649b94503b5084d313a2e55
Shae Smith <sbsmith86@gmail.com>
2016-03-30 13:42:20 -0400

    add leaderboard message field to split form Move split form into it's own partial

---
+++ resources/views/waitingrooms/partials/_form_split.blade.php
@@ -0,0 +1,29 @@
+<form method="POST" action="{{ route('split', $room->id) }}">
+    {{ method_field('POST') }}
+    {{ csrf_field() }}
+
+    <div class="container__block">
+        <div class="form-item -padded">
+            <div class="form-item -padded">
+                <label class="field-label" for="competition_end_date">End date for these competitions:</label>
+                <input type="date" name="competition_end_date" id="competition_end_date" value='MM/DD/YYYY' class="text-field"></input>
+            </div>
+            <div class="form-item -padded">
+                <label class="field-label" for="leaderboard_msg_day">Send leaderboard message on:</label>
+                <div class="select">
+                    <select name="leaderboard_msg_day">
+                        <option value="1">Monday</option>
+                        <option value="2">Tuesday</option>
+                        <option value="3">Wednesday</option>
+                        <option value="4">Thursday</option>
+                        <option value="5">Friday</option>
+                        <option value="6">Saturday</option>
+                        <option value="7">Sunday</option>
+                    </select>
+                </div>
+            </div>
+        </div>
+
+        <input type="submit" class="button" value="Split" />
+    </div>
+</form>
