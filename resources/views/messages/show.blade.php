6d2df810444ef9fae149d3e36d3198180f87e925
Andrea Gaither <angaither@gmail.com>
2016-04-01 12:16:27 -0400

    Added edit links with hashes to edit the message before sending.

--- resources/views/messages/show.blade.php
+++ resources/views/messages/show.blade.php
@@ -7,11 +7,10 @@
         'subtitle' => 'Viewing messages in contest ' . $messages[0]->contest_id
     ])
 
-
      <div class="container">
         <div class="wrapper">
             <div class="container__block">
-                <h2 class="heading -banner">Messages</h2>
+                <h2 class="heading -banner">Send Messages</h2>
                 <table class="table">
                     <thead>
                         <tr class="table__header">
@@ -24,7 +23,7 @@
                     <tbody>
                         @foreach ($messages as $message)
                             <tr class="table__row">
-                                <td class="table__cell">{{ $message->label or 'N/A'}}</td>
+                                <td class="table__cell"> <a href="{{ route('messages.edit', $message->contest_id)}}#{{ $message->type . $message->key }}">{{ $message->label or 'N/A'}}</a></td>
                                 <td class="table__cell">{{ $message->subject or 'N/A'}}</td>
                                 <td class="table__cell">{{ str_limit($message->body, 100) }}</td>
                                 <td class="table__cell"><a href="{{ route('messages.send', ['message' => $message->id, 'contest_id' => $message->contest_id, 'competition_id' => $competition->id]) }}" class="button -tertiary ">Send</a></td>
