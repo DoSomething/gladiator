41a5ccfa9559284a43f9753cd82bb35511a69f26
Shae Smith <sbsmith86@gmail.com>
2016-04-01 11:58:24 -0400

    Use leaderboard_msg_day field partial in the split form

--- resources/views/waitingrooms/partials/_form_split.blade.php
+++ resources/views/waitingrooms/partials/_form_split.blade.php
@@ -8,20 +8,10 @@
                 <label class="field-label" for="competition_end_date">End date for these competitions:</label>
                 <input type="date" name="competition_end_date" id="competition_end_date" value='MM/DD/YYYY' class="text-field"></input>
             </div>
-            <div class="form-item -padded">
-                <label class="field-label" for="leaderboard_msg_day">Send leaderboard message on:</label>
-                <div class="select">
-                    <select name="leaderboard_msg_day">
-                        <option value="0">Monday</option>
-                        <option value="1">Tuesday</option>
-                        <option value="2">Wednesday</option>
-                        <option value="3">Thursday</option>
-                        <option value="4">Friday</option>
-                        <option value="5">Saturday</option>
-                        <option value="6">Sunday</option>
-                    </select>
-                </div>
-            </div>
+
+            @include('competitions.partials._form_leaderboard_msg_day_field', [
+                'default' => null,
+            ])
         </div>
 
         <input type="submit" class="button" value="Split" />
