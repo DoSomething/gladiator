179b0efc62bde6e946eed22320bfa96afc08669b
Andrea Gaither <angaither@gmail.com>
2016-04-08 16:00:09 -0400

    Adding a fake model to see what styleci does.

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace Gladiator\Models;
 
 use Gladiator\Services\Northstar\Exceptions\NorthstarUserNotFoundException;
 use Illuminate\Foundation\Auth\User as BaseUser;
+use Gladiator\Models\Message;
 
 class User extends BaseUser
 {
