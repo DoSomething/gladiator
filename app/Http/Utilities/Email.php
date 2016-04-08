ce673f19e3242753475334a8feb6d191ddc70dee
Shae Smith <sbsmith86@gmail.com>
2016-04-08 16:04:47 -0400

    add prove it link token

--- app/Http/Utilities/Email.php
+++ app/Http/Utilities/Email.php
@@ -76,6 +76,7 @@ class Email
             ':reportback_noun:'       => $this->contest->campaign->reportback_info->noun,
             ':reportback_verb:'       => $this->contest->campaign->reportback_info->verb,
             ':pro_tip:'               => $this->message->pro_tip,
+            ':prove_it_link:'         => url(config('services.phoenix.uri') .'/node/' . $this->contest->campaign_id . '#proveit'),
         ];
 
         return $tokens;
