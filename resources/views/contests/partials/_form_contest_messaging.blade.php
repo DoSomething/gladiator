8fc350e48dc8894e8ebec8e258a984486b1b3331
Shae Smith <sbsmith86@gmail.com>
2016-04-14 11:15:59 -0400

    add default signoff

--- resources/views/contests/partials/_form_contest_messaging.blade.php
+++ resources/views/contests/partials/_form_contest_messaging.blade.php
@@ -12,14 +12,16 @@
     </div>
 
     @if ($message['type'] === 'leaderboard')
-        <label class="field-label" for="{{ correspondence()->getAttribute($message, 'pro_tip') }}">Pro Tip:</label>
-        <textarea class="text-field" name="{{ correspondence()->getAttribute($message, 'pro_tip') }}" id="{{ correspondence()->getAttribute($message, 'pro_tip') }}" rows="3">{{ correspondence($message, 'pro_tip') }}</textarea>
-    @endif
+        <div class="form-item -padded">
+            <label class="field-label" for="{{ correspondence()->getAttribute($message, 'pro_tip') }}">Pro Tip:</label>
+            <textarea class="text-field" name="{{ correspondence()->getAttribute($message, 'pro_tip') }}" id="{{ correspondence()->getAttribute($message, 'pro_tip') }}" rows="3">{{ correspondence($message, 'pro_tip') }}</textarea>
+        </div>
 
-    <div class="form-item -padded">
-        <label class="field-label" for="{{ correspondence()->getAttribute($message, 'signoff') }}">Signoff:</label>
-        <textarea class="text-field" name="{{ correspondence()->getAttribute($message, 'signoff') }}" id="{{ correspondence()->getAttribute($message, 'signoff') }}" rows="10">{{ correspondence($message, 'signoff') }}</textarea>
-    </div>
+        <div class="form-item -padded">
+            <label class="field-label" for="{{ correspondence()->getAttribute($message, 'signoff') }}">Signoff:</label>
+            <textarea class="text-field" name="{{ correspondence()->getAttribute($message, 'signoff') }}" id="{{ correspondence()->getAttribute($message, 'signoff') }}" rows="10">{{ correspondence($message, 'signoff') }}</textarea>
+        </div>
+    @endif
 
     <input type="hidden" name="{{ correspondence()->getAttribute($message, 'label') }}" value="{{ correspondence($message, 'label') }}" />
 </fieldset>
