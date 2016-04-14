2ff85aca102e9a4ad0414db0bd594aafdd3a9f17
Shae Smith <sbsmith86@gmail.com>
2016-04-14 10:14:44 -0400

    Add signoff field to message edit form

--- resources/views/contests/partials/_form_contest_messaging.blade.php
+++ resources/views/contests/partials/_form_contest_messaging.blade.php
@@ -16,5 +16,10 @@
         <textarea class="text-field" name="{{ correspondence()->getAttribute($message, 'pro_tip') }}" id="{{ correspondence()->getAttribute($message, 'pro_tip') }}" rows="3">{{ correspondence($message, 'pro_tip') }}</textarea>
     @endif
 
+    <div class="form-item -padded">
+        <label class="field-label" for="{{ correspondence()->getAttribute($message, 'signoff') }}">Signoff:</label>
+        <textarea class="text-field" name="{{ correspondence()->getAttribute($message, 'signoff') }}" id="{{ correspondence()->getAttribute($message, 'signoff') }}" rows="10">{{ correspondence($message, 'signoff') }}</textarea>
+    </div>
+
     <input type="hidden" name="{{ correspondence()->getAttribute($message, 'label') }}" value="{{ correspondence($message, 'label') }}" />
 </fieldset>
