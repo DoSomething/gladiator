fab26387ad934faee4fee7afa7e56f7ce21e3eab
Shae Smith <sbsmith86@gmail.com>
2016-04-14 10:10:01 -0400

    Add signoff field to messages table

---
+++ database/migrations/2016_04_14_140824_add_signoff_to_messages.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddSignoffToMessages extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('messages', function (Blueprint $table) {
+            $table->longtext('signoff')->nullable()->after('body');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('messages', function (Blueprint $table) {
+            $table->dropColumn('signoff');
+        });
+    }
+}
