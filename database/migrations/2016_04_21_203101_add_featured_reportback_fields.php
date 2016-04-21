b6f830077b9abb3f67543802c7e8ab160e215511
Andrea Gaither <angaither@gmail.com>
2016-04-21 17:31:21 -0400

    Created feature reportback migration.

---
+++ database/migrations/2016_04_21_203101_add_featured_reportback_fields.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddFeaturedReportbackFields extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+       Schema::table('messages', function (Blueprint $table) {
+            $table->integer('reportback_id')->nullable()->after('pro_tip');
+            $table->integer('reportback_item_id')->nullable()->after('reportback_id');
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('messages', function (Blueprint $table) {
+            $table->dropColumn('reportback_id');
+            $table->dropColumn('reportback_item_id');
+        });
+    }
+}
