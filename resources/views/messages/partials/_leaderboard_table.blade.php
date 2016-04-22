5eba1e7d71c5c53fa97b8fdbee42e79cc1a571e8
Shae Smith <sbsmith86@gmail.com>
2016-04-21 23:40:57 -0400

    basic style of the table

--- resources/views/messages/partials/_leaderboard_table.blade.php
+++ resources/views/messages/partials/_leaderboard_table.blade.php
@@ -2,18 +2,18 @@
 
 <table class="table">
     <thead>
-        <tr class="table__header">
-            <th class="table__cell">Rank</th>
-            <th class="table__cell">Name</th>
-            <th class="table__cell">Quantity</th>
+        <tr class="table__header" style="border-collapse: collapse; width: 100%">
+            <th class="table__cell" style="border-bottom: 1px solid #ddd; padding: 10px">Rank</th>
+            <th class="table__cell" style="border-bottom: 1px solid #ddd; padding: 10px">Name</th>
+            <th class="table__cell" style="border-bottom: 1px solid #ddd; padding: 10px">Quantity</th>
         </tr>
     </thead>
     <tbody>
         @foreach ($leaderboard as $user)
             <tr class="table__row">
-                <td class="table__cell">{{ $user['rank'] }}</td>
-                <td class="table__cell">{{ $user['first_name'] or 'Anonymous' }} {{ $user['last_initial'] or '' }}</td>
-                <td class="table__cell">{{ $user['reportback']['quantity'] or 'n/a' }}</td>
+                <td class="table__cell" style="border-bottom: 1px solid #ddd; padding: 10px">{{ $user['rank'] }}</td>
+                <td class="table__cell" style="border-bottom: 1px solid #ddd; padding: 10px">{{ $user['first_name'] or 'Anonymous' }} {{ $user['last_initial'] or '' }}</td>
+                <td class="table__cell" style="border-bottom: 1px solid #ddd; padding: 10px">{{ $user['reportback']['quantity'] or 'n/a' }}</td>
             </tr>
         @endforeach
     </tbody>
