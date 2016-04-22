8612f1d25c8a6444fc5be7052f5bb426ebc7b908
Diego Lorenzo <diego@diegolorenzo.com>
2016-04-22 11:45:38 -0400

    Adding new UpdateMessages console command.

---
+++ app/Console/Commands/UpdateMessages.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Gladiator\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class UpdateMessages extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'update:messages {contest?}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        //
+    }
+}
