5d18892385e047d826cb361330f3a6f47d840f06
deadlybutter <thedeadlybutter@gmail.com>
2016-04-26 13:21:09 -0400

    Changes migration to use competition, not contest

--- database/migrations/2016_04_26_151130_add_rules_to_competition.php
+++ database/migrations/2016_04_26_151130_add_rules_to_competition.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddRulesToContest extends Migration
+class AddRulesToCompetition extends Migration
 {
     /**
      * Run the migrations.
@@ -12,7 +12,7 @@ class AddRulesToContest extends Migration
      */
     public function up()
     {
-        Schema::table('contests', function (Blueprint $table) {
+        Schema::table('competitions', function (Blueprint $table) {
             $table->string('rules');
         });
     }
@@ -24,7 +24,7 @@ class AddRulesToContest extends Migration
      */
     public function down()
     {
-        Schema::table('contests', function (Blueprint $table) {
+        Schema::table('competitions', function (Blueprint $table) {
             $table->dropColumn('rules');
         });
     }
