45d4fc3373325f0daa95bd33b7b662520f883643
deadlybutter <thedeadlybutter@gmail.com>
2016-04-26 11:15:56 -0400

    Adds migration for rules column on contest table

---
+++ database/migrations/2016_04_26_151130_add_rules_to_contest.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddRulesToContest extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('contests', function (Blueprint $table) {
+            $table->string('rules');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('contests', function (Blueprint $table) {
+            $table->dropColumn('rules');
+        });
+    }
+}
