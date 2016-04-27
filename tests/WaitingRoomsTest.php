cedbb45ac3278f958f7aecba7c794668fd20f4e5
deadlybutter <thedeadlybutter@gmail.com>
2016-04-27 15:55:19 -0400

    Adds waiting room test case for default split

---
+++ tests/WaitingRoomsTest.php
@@ -0,0 +1,53 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Carbon\Carbon;
+use Gladiator\Models\Contest;
+use Gladiator\Models\WaitingRoom;
+use Gladiator\Models\User;
+
+class WaitingRoomsTest extends TestCase
+{
+
+    use DatabaseMigrations;
+    use Illuminate\Foundation\Testing\WithoutMiddleware;
+
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testDefaultWaitingRoomSplit()
+    {
+        // Create contest
+        $contest = Contest::create([
+            'campaign_id' => 5,
+            'campaign_run_id' => 10,
+            'sender_email' => 'epictestemail@fantini.com',
+            'sender_name' => '@george',
+        ]);
+
+        // Create waiting room
+        $contest->waitingRoom->signup_start_date = Carbon::now()->startOfDay();
+        $contest->waitingRoom->signup_end_date = Carbon::now()->addWeeks(3)->endOfDay();
+        $contest->waitingRoom->save();
+
+        // Add 600 people
+        $users = factory(User::class, 600)->create();
+        foreach ($users as $user) {
+            $contest->waitingRoom->users()->attach($user->id);
+        }
+
+        $this->visit(route('split', [$contest->waitingRoom->id]))
+            ->type(Carbon::now()->addWeeks(3)->endOfDay(), 'competition_end_date')
+            ->type(300, 'competition_max')
+            ->type('http://docs.google.com/lol', 'rules_url')
+            ->press('Split');
+
+        $this->see('Waiting Room has been split!');
+
+        $this->assertTrue(count($contest->competitions->toArray()) ==  2);
+    }
+}
