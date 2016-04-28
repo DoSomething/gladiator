3902ac264befaef119c8baa158e758f89321350e
Shae Smith <sbsmith86@gmail.com>
2016-04-28 10:47:04 -0400

    Use faker to generate test info

--- tests/ContestsTest.php
+++ tests/ContestsTest.php
@@ -14,20 +14,23 @@ class ContestsTest extends TestCase
 	 */
 	public function testCreatingAContest()
 	{
-		$signup_start_date = Carbon::now()->startOfDay();
-		$signup_end_date = Carbon::now()->addMonth()->endOfDay();
+		$signupStartDate = Carbon::now()->startOfDay();
+		$signupEndDate = Carbon::now()->addMonth()->endOfDay();
+		$senderName = $this->faker->firstName;
+		$senderEmail = $this->faker->unique()->safeEmail;
+		$campaignID = $this->faker->randomNumber(4);
+		$campaignRunID = $this->faker->randomNumber(3);
 
 		// Fill out form.
 		$this->asAdminUser()
 		->visit(route('contests.create'))
 		->see('Add a new contest')
-		->type($signup_start_date, 'signup_start_date')
-		->type($signup_end_date, 'signup_end_date')
-		// @TODO - use faker to build dumb email and name and stuff.
-		->type('1234', 'campaign_id')
-		->type('123', 'campaign_run_id')
-		->type('ssmith@dosomething.org', 'sender_email')
-		->type('Shae', 'sender_name')
+		->type($signupStartDate, 'signup_start_date')
+		->type($signupEndDate, 'signup_end_date')
+		->type($campaignID, 'campaign_id')
+		->type($campaignRunID, 'campaign_run_id')
+		->type($senderEmail, 'sender_email')
+		->type($senderName, 'sender_name')
 		->press('Submit');
 
 		$this->see('Contest has been saved!');
@@ -35,18 +38,18 @@ class ContestsTest extends TestCase
 		// Make sure contest was created.
         $this->seeInDatabase('contests', [
             'id' => '1',
-            'campaign_id' => '1234',
-            'campaign_run_id' => '123',
-            'sender_email' => 'ssmith@dosomething.org',
-            'sender_name' => 'Shae',
+            'campaign_id' => $campaignID,
+            'campaign_run_id' => $campaignRunID,
+            'sender_email' => $senderEmail,
+            'sender_name' =>$senderName,
         ]);
 
         // Make sure a waiting room was created.
         $this->seeInDatabase('waiting_rooms', [
 			'id' => '1',
 			'contest_id' => '1',
-			'signup_start_date' => $signup_start_date,
-			'signup_end_date' => $signup_end_date,
+			'signup_start_date' => $signupStartDate,
+			'signup_end_date' => $signupEndDate,
         ]);
 
         // Make sure default messages are created. Just test that required fields are there.
