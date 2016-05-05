cc7b29db47fe00456b94a06bb54776a76180d431
deadlybutter <thedeadlybutter@gmail.com>
2016-05-05 11:13:34 -0400

    changing button

--- resources/views/messages/show.blade.php
+++ resources/views/messages/show.blade.php
@@ -16,9 +16,9 @@
                         <tr class="table__header">
                             <th class="table__cell">Label</th>
                             <th class="table__cell">Subject</th>
+                            <th class="table_cell">Featured Reportback</th>
                             <th class="table__cell">Test Send</th>
                             <th class="table__cell">Live Send</th>
-                            <th class="table_cell">Featured Reportback</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -26,13 +26,13 @@
                             <tr class="table__row">
                                 <td class="table__cell"> <a href="{{ route('messages.edit', $message->contest_id)}}#{{ $message->type . $message->key }}">{{ $message->label or 'N/A'}}</a></td>
                                 <td class="table__cell">{{ $message->subject or 'N/A'}}</td>
-                                <td class="table__cell"><a href="{{ route('messages.send', ['message' => $message->id, 'contest_id' => $message->contest_id, 'competition_id' => $competition->id, 'test' => true, ]) }}" class="button -tertiary">Test</a></td>
-                                <td class="table__cell"><a href="{{ route('messages.send', ['message' => $message->id, 'contest_id' => $message->contest_id, 'competition_id' => $competition->id]) }}" class="button -tertiary">Send</a></td>
                                 <td class="table__cell">
                                     @if ($message->type === "leaderboard")
-                                        <a href="{{ route('competitions.featuredReportbackForm', ['competition' => $competition->id, 'message' => $message->id]) }}" class="button {{ isset($featuredReportbacks[$message->id]) ? '-tertiary' : '-secondary' }}">EDIT</a>
+                                        <a href="{{ route('competitions.featuredReportbackForm', ['competition' => $competition->id, 'message' => $message->id]) }}" class="button -secondary">{{ isset($featuredReportbacks[$message->id]) ? 'Edit' : 'Set' }}</a>
                                     @endif
                                 </td>
+                                <td class="table__cell"><a href="{{ route('messages.send', ['message' => $message->id, 'contest_id' => $message->contest_id, 'competition_id' => $competition->id, 'test' => true, ]) }}" class="button -tertiary">Test</a></td>
+                                <td class="table__cell"><a href="{{ route('messages.send', ['message' => $message->id, 'contest_id' => $message->contest_id, 'competition_id' => $competition->id]) }}" class="button -tertiary">Send</a></td>
                             </tr>
                         @endforeach
                     </tbody>
