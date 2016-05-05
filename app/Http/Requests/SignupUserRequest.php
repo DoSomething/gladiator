d9be7ab1e1470e34973632ab63401536173bdf06
Andrea Gaither <angaither@gmail.com>
2016-05-05 11:44:45 -0400

    The matching request class for retroactivly adding a user to a room/comp
    Fixes #302

---
+++ app/Http/Requests/SignupUserRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Gladiator\Http\Requests;
+
+use Gladiator\Http\Requests\Request;
+
+class SignupUserRequest extends Request
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'id' => 'required',
+        ];
+    }
+}
