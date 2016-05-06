6879279ba85f2c25e108ad31da238c113882f24e
deadlybutter <thedeadlybutter@gmail.com>
2016-05-06 11:03:39 -0400

    just null

--- database/seeds/MessageSettingTableSeeder.php
+++ database/seeds/MessageSettingTableSeeder.php
@@ -22,7 +22,7 @@ class MessageSettingTableSeeder extends Seeder
             $setting->subject = $message['subject'];
             $setting->body = $message['body'];
             $setting->pro_tip = $message['pro_tip'];
-            $setting->signoff = $message['signoff'];
+            $setting->signoff = NULL;
             $setting->save();
         }
     }
