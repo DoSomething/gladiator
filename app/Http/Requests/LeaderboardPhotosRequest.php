aacd88439644f16e621274925ae74d6b181cd22b
Linda Ortega Cordoves <lcordoves@dosomething.org>
2016-10-12 15:48:24 +0000

    333: Changed name of Leaderboard Photos request file

---
+++ app/Http/Requests/LeaderboardPhotosRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Gladiator\Http\Requests;
+
+use Gladiator\Http\Requests\Request;
+
+class LeaderboardPhotosRequest extends Request
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+      return [
+          'reportback_id' => 'required|numeric',
+          'reportback_item_id' => 'required|numeric',
+      ];
+    }
+}
