0909e163b8f2736075dd4ae97188d66ce46635cc
Linda Ortega Cordoves <lcordoves@dosomething.org>
2016-10-12 15:36:40 +0000

    333: Created leaderboard_photos migration, model, and request

---
+++ app/Models/LeaderboardPhotos.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Gladiator;
+
+use Illuminate\Database\Eloquent\Model;
+
+class LeaderboardPhotos extends Model
+{
+  /**
+   * Get the fillable attributes for the model.
+   *
+   * @return array
+   */
+  protected $fillable = ['reportback_id', 'reportback_item_id'];
+
+  /**
+   * A LeaderboardPhotos belongs to one user.
+   */
+  public function user()
+  {
+      return $this->belongsTo(User::class);
+  }
+
+  /**
+   * A LeaderboardPhotos belongs to one competition.
+   */
+  public function competition()
+  {
+      return $this->belongsTo(Competition::class);
+  }
+
+  /**
+   * A LeaderboardPhotos belongs to one message.
+   */
+  public function message()
+  {
+      return $this->belongsTo(Message::class);
+  }
+}
