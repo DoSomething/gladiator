9e813f6b19de0ce06ce52d221091d6ef0cd71750
Shae Smith <sbsmith86@gmail.com>
2017-02-15 14:53:56 -0500

    Update foreign keys when renaming columns

--- database/migrations/2017_02_07_203017_rename_id_to_northstar_id.php
+++ database/migrations/2017_02_07_203017_rename_id_to_northstar_id.php
@@ -15,6 +15,24 @@ class RenameIdToNorthstarId extends Migration
         Schema::table('users', function (Blueprint $table) {
             $table->renameColumn('id', 'northstar_id');
         });
+
+        Schema::table('user_waiting_room', function (Blueprint $table) {
+             $table->dropForeign('user_waiting_room_user_id_foreign');
+             $table->renameColumn('user_id', 'northstar_id');
+             $table->foreign('northstar_id')->references('northstar_id')->on('users')->onDelete('cascade');
+        });
+
+        Schema::table('competition_user', function (Blueprint $table) {
+             $table->dropForeign('competition_user_user_id_foreign');
+             $table->renameColumn('user_id', 'northstar_id');
+             $table->foreign('northstar_id')->references('northstar_id')->on('users')->onDelete('cascade');
+        });
+
+        Schema::table('leaderboard_photos', function (Blueprint $table) {
+             $table->dropForeign('leaderboard_photos_user_id_foreign');
+             $table->renameColumn('user_id', 'northstar_id');
+             $table->foreign('northstar_id')->references('northstar_id')->on('users')->onDelete('cascade');
+        });
     }
 
     /**
@@ -26,6 +44,24 @@ class RenameIdToNorthstarId extends Migration
     {
         Schema::table('users', function (Blueprint $table) {
             $table->renameColumn('northstar_id', 'id');
+        });
+
+        Schema::table('user_waiting_room', function (Blueprint $table) {
+            $table->dropForeign('user_waiting_room_northstar_id_foreign');
+            $table->renameColumn('northstar_id', 'user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+
+        Schema::table('competition_user', function (Blueprint $table) {
+             $table->dropForeign('competition_user_northstar_id_foreign');
+             $table->renameColumn('northstar_id', 'user_id');
+             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+
+        Schema::table('leaderboard_photos', function (Blueprint $table) {
+             $table->dropForeign('leaderboard_photos_northstar_id_foreign');
+             $table->renameColumn('northstar_id', 'user_id');
+             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
     }
 }
