5021eed0c0b57036621263de6ab3a5bf0c8d1934
ScottPaillant <scottpaillant@gmail.com>
2017-03-28 17:22:47 -0400

    Revert "On first storage of competition overviews sessions, correctly grab unsubscribed users"
    This reverts commit 4d45989a94d82690405b41d960f639f4d8be68d5.

--- app/Http/Controllers/CompetitionsController.php
+++ app/Http/Controllers/CompetitionsController.php
@@ -58,7 +58,7 @@ class CompetitionsController extends Controller
 
             session()->reflash();
         } else {
-            $competition = $this->manager->getCompetitionOverview($competition, true, true);
+            $competition = $this->manager->getCompetitionOverview($competition, true);
         }
 
         $statistics = $this->manager->getStatisticsForCompetition($competition);
@@ -130,7 +130,7 @@ class CompetitionsController extends Controller
         if (session()->has($key)) {
             $competition = session($key);
         } else {
-            $competition = $this->manager->getCompetitionOverview($competition, $withReportback, true);
+            $competition = $this->manager->getCompetitionOverview($competition, $withReportback);
         }
 
         if (! $withReportback) {
@@ -174,7 +174,7 @@ class CompetitionsController extends Controller
 
             session()->reflash();
         } else {
-            $competition = $this->manager->getCompetitionOverview($competition, true, true);
+            $competition = $this->manager->getCompetitionOverview($competition, true);
 
             session()->flash($key, $competition);
         }
@@ -200,7 +200,7 @@ class CompetitionsController extends Controller
 
             session()->reflash();
         } else {
-            $competition = $this->manager->getCompetitionOverview($competition, true, true);
+            $competition = $this->manager->getCompetitionOverview($competition, true);
 
             session()->flash($key, $competition);
         }
@@ -265,7 +265,7 @@ class CompetitionsController extends Controller
             $competition = session($key);
             session()->reflash();
         } else {
-            $competition = $this->manager->getCompetitionOverview($competition, true, true);
+            $competition = $this->manager->getCompetitionOverview($competition, true);
         }
 
         $leaderboard = $competition->activity['active'];
