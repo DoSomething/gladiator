3e274f1a3b74cc6391789850f2a5083c74be7da0
Shae Smith <sbsmith86@gmail.com>
2017-08-03 16:23:48 -0400

    Add new contest transformer

---
+++ app/Http/Transformers/ContestTransformer.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Gladiator\Http\Transformers;
+
+use Gladiator\Models\Contest;
+use League\Fractal\TransformerAbstract;
+
+class ContestTransformer extends TransformerAbstract
+{
+    /**
+     * Transform resource data.
+     *
+     * @param  Contest  $contest
+     * @return array
+     */
+    public function transform(Contest $contest)
+    {
+        return [
+            'id' => $contest->id,
+            'campaign_id' => $contest->campaign_id,
+            'campaign_run_id' => $contest->campaign_run_id,
+            'sender' => [
+                "name" => $contest->sender_name,
+                "email" => $contest->sender_email,
+            ],
+            'created_at' => $contest->created_at->toIso8601String(),
+            'updated_at' => $contest->updated_at->toIso8601String(),
+            'waiting_room' => [
+                'open' => $contest->waitingRoom->isOpen(),
+                'signup_dates' => [
+                    'start' => $contest->waitingRoom->signup_start_date->toIso8601String(),
+                    'end' => $contest->waitingRoom->signup_end_date->toIso8601String(),
+                ],
+            ],
+        ];
+    }
+}
